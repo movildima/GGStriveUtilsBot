86ac9c239cf7903482e0b32f1e0aebb52f87cc2f
movildima <movildima@gmail.com>
2021-07-04 13:35:55 +0200

    Add project files.

---
+++ GGStriveUtilsBot/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+
+namespace GGStriveUtilsBot
+{
+    class Program
+    {
+        static DiscordClient discord;
+        static CommandsNextExtension commands;
+
+        static void Main(string[] args)
+        {
+            MainAsync().GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync()
+        {
+            discord = new DiscordClient(new DiscordConfiguration()
+            {
+                Token = "Token",
+                TokenType = TokenType.Bot
+            });
+
+            await discord.ConnectAsync();
+            await Task.Delay(-1);
+        }
+    }
+}
