60b06a5cfa02ad09232fb5f9957e75a5f20c6ab0
movildima <movildima@gmail.com>
2021-11-20 13:10:29 +0100

    fixed bad logic around goldlewis level moves

--- GGStriveUtilsBot/Utils/DustloopDataFetcher.cs
+++ GGStriveUtilsBot/Utils/DustloopDataFetcher.cs
@@ -175,10 +175,9 @@ namespace GGStriveUtilsBot.Utils
                         "thunderbird", "skyfish", "burn it down",
                         "214s", "236s", "236236k"
                     };
-                if (levelMoves.GetRange(0, 3).Any(f => Levenshtein.Distance(f, move) < LDistance) ||
-                    (levelMoves.GetRange(3, 3).Any(f => f == move) &&
-                    character.HasValue ? (Character)character == Character.Goldlewis : false &&
-                    level.Length == 0))
+                if ((levelMoves.GetRange(0, 3).Any(f => Levenshtein.Distance(f, move) < LDistance) ||
+                    (levelMoves.GetRange(3, 3).Any(f => f == move) && character.HasValue ? (Character)character == Character.Goldlewis : false)) &&
+                    level.Length == 0)
                     return (character, move, "level", isNumpad);
             }
 
