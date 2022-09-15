4807d6bc1657c6c7640c763c68ccb2ddafcada42
movildima <movildima@gmail.com>
2022-09-15 10:46:37 +0200

    removed character selection from the slash command

--- GGStriveUtilsBot/SlashCommands/FrameDataSlashModule.cs
+++ GGStriveUtilsBot/SlashCommands/FrameDataSlashModule.cs
@@ -12,10 +12,13 @@ namespace GGStriveUtilsBot.SlashCommands
     class FrameDataSlashModule : ApplicationCommandModule
     {
         [SlashCommand("framedata", "Fetch frame data of a specified move from Dustloop wiki.")]
-        public async Task FrameDataCommand(InteractionContext ctx, [Option("Character", "Character in question")] Character character, [Option("Move", "Move name or numpad notation of it")] string move)
+        public async Task FrameDataCommand(InteractionContext ctx, /*[Option("Character", "Character in question")] Character character, */[Option("Request", "(Optional) Character name, followed by move name or numpad notation of it")] string move)
         {
             await ctx.CreateResponseAsync(InteractionResponseType.DeferredChannelMessageWithSource);
-            var result = await Utils.FrameDataEmbedBuilder.selectEmbed(ctx.Client, ctx.User, ctx.Channel, character.GetName() + " " + move);
+            //var result = await Utils.FrameDataEmbedBuilder.selectEmbed(ctx.Client, ctx.User, ctx.Channel, character.GetName() + " " + move);
+            //if (result != null)
+            //    await ctx.EditResponseAsync(new DiscordWebhookBuilder().AddEmbed(result));
+            var result = await Utils.FrameDataEmbedBuilder.selectEmbed(ctx.Client, ctx.User, ctx.Channel, move);
             if (result != null)
                 await ctx.EditResponseAsync(new DiscordWebhookBuilder().AddEmbed(result));
             else
